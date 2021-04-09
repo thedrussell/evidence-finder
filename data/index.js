acb85af114daf0b0adafa8565d2f5d2fa57a1820
Jon Barker <jon@inktrap.co.uk>
2021-04-09 11:45:36 +0100

    Trim data coming in

--- data/index.js
+++ data/index.js
@@ -36,9 +36,9 @@ fs.createReadStream('data.csv')
       mainObject = filteredByID[0];
 
       filteredByID.forEach((row, i) => {
-        outcomes.includes(row.outcomes) ? null : outcomes.push(row.outcomes);
-        interventions.includes(row.interventions) ? null : interventions.push(row.interventions);
-        populations.includes(row.populations) ? null : populations.push(row.populations);
+        outcomes.includes(row.outcomes) ? null : outcomes.push(row.outcomes.trim());
+        interventions.includes(row.interventions) ? null : interventions.push(row.interventions.trim());
+        populations.includes(row.populations) ? null : populations.push(row.populations.trim());
       });
 
 
@@ -46,25 +46,25 @@ fs.createReadStream('data.csv')
           "type": "Feature",
           "properties": {
             "unique_id": mainObject.unique_id,
-            "type": mainObject.map,
-            "studyDesign": mainObject.study_design,
-            "studyType": mainObject.type_of_study,
+            "type": mainObject.map.trim(),
+            "studyDesign": mainObject.study_design.trim(),
+            "studyType": mainObject.type_of_study.trim(),
             "title": mainObject.title.trim(),
             "year": mainObject.year.trim(),
             "yearGroup": (mainObject.year.length) ? getYearGroup(mainObject.year) : "",
             "url": mainObject.url,
-            "authors": mainObject.authors,
+            "authors": mainObject.authors.trim(),
             "interventionCategories": interventions,
             "populationGroups": populations,
-            "abstract": mainObject.abstract,
+            "abstract": mainObject.abstract.trim(),
             "outcomes": outcomes,
-            "city": mainObject.city,
-            "status": mainObject.status,
-            "countryX": mainObject.country_x,
-            "countryY": mainObject.country_y,
-            "stateX": mainObject.region___state_x,
-            "stateY": mainObject.region___state_y,
-            "quality": mainObject.confidence_in_study_findings,
+            "city": mainObject.city.trim(),
+            "status": mainObject.status.trim(),
+            "countryX": mainObject.country_x.trim(),
+            "countryY": mainObject.country_y.trim(),
+            "stateX": mainObject.region___state_x.trim(),
+            "stateY": mainObject.region___state_y.trim(),
+            "quality": mainObject.confidence_in_study_findings.trim(),
           },
           "geometry": {
             "type": "Point",
